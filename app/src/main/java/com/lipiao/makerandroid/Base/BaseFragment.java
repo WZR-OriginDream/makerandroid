a285fc21d5cbc7e98a84aae8c9640e41596f6328
lipiaoMKX <2357390156@qq.com>
2019-08-22 23:32:09 +0800

    android更新

--- app/src/main/java/com/lipiao/makerandroid/Base/BaseFragment.java
+++ app/src/main/java/com/lipiao/makerandroid/Base/BaseFragment.java
@@ -11,13 +11,10 @@ import android.view.ViewGroup;
 
 /**
  * Fragment的基类
- * 1.选择表示层
- * 2.实现懒加载
+ * 实现懒加载
  */
 
-public abstract class BaseFragment<V, T extends BasePresenter<V>> extends Fragment {
-    //表示层的引用
-    public T bannerPresenter;
+public abstract class BaseFragment extends Fragment {
 
     public final String TAG = getClass().getSimpleName();
     public boolean mHaveLoadData; // 表示是否已经请求过数据
@@ -27,60 +24,22 @@ public abstract class BaseFragment<V, T extends BasePresenter<V>> extends Fragme
     // 表示开始加载数据, 但不表示数据加载已经完成
     public abstract void loadDataStart();
 
-    // 表示找控件完成, 给控件们设置数据不会报空指针了
-    public boolean mViewInflateFinished;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView");
+        //已经加载过则直接使用
         if (mRootView != null) {
             return mRootView;
         }
         mRootView = initRootView(inflater, container, savedInstanceState);
-        findViewById(mRootView);
-        mViewInflateFinished = true;
         return mRootView;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        Log.d(TAG, "onViewCreated");
-    }
-
-    protected abstract void findViewById(View view);
 
     protected abstract View initRootView(LayoutInflater inflater, ViewGroup container,
                                          Bundle savedInstanceState);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //绑定
-        bannerPresenter = createPresenter();
-        bannerPresenter.attachView((V) this);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //解绑
-        bannerPresenter.detachView((V) this);
-    }
-
-    @Override
-    public void onDestroyView() {
-        Log.d(TAG, "onDestroyView");
-        super.onDestroyView();
-    }
-
-    @Override
-    public void onDetach() {
-        Log.d(TAG, "onDetach");
-        super.onDetach();
-    }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
@@ -94,8 +53,7 @@ public abstract class BaseFragment<V, T extends BasePresenter<V>> extends Fragme
         }
     }
 
-
-    //提供回调方法，让子类fragment选择表示层
-    protected abstract T createPresenter();
+//    //提供回调方法，让子类fragment选择表示层
+//    protected abstract T createPresenter();
 }
 
