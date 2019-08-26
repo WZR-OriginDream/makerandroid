c8e2098b070b18457df5c5a535dc2b04f3d68fab
lipiaoMKX <2357390156@qq.com>
2019-08-26 17:14:03 +0800

    android更新

--- app/src/main/java/com/lipiao/makerandroid/View/Fragment/UserFragment.java
+++ app/src/main/java/com/lipiao/makerandroid/View/Fragment/UserFragment.java
@@ -87,6 +87,7 @@ public class UserFragment extends Fragment {
         mAgentWeb = AgentWeb.with(this)//
                 .setAgentWebParent((LinearLayout) linearLayout, -1, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))//传入AgentWeb的父控件。
                 .useDefaultIndicator(-1, 3)//设置进度条颜色与高度，-1为默认值，高度为2，单位为dp。
+                .setWebChromeClient(mWebChromeClient)
                 .setSecurityType(AgentWeb.SecurityType.STRICT_CHECK) //严格模式 Android 4.2.2 以下会放弃注入对象 ，使用AgentWebView没影响。
                 .setMainFrameErrorView(R.layout.agentweb_error_page, -1) //参数1是错误显示的布局，参数2点击刷新控件ID -1表示点击整个布局都刷新， AgentWeb 3.0.0 加入。
                 .setOpenOtherPageWays(DefaultWebClient.OpenOtherPageWays.ASK)//打开其他页面时，弹窗质询用户前往其他应用 AgentWeb 3.0.0 加入。
